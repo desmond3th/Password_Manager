0bed60b19a7f1c132f12ea328c802acddcb5b13b
Saurabh Kumar <106867366+desmond3th@users.noreply.github.com>
2023-07-07 00:23:02 +0530

    Update PasswordManager.java

--- PasswordManager.java
+++ PasswordManager.java
@@ -36,7 +36,7 @@ class PasswordGenerator {
     private static final String caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private static final String small_caps = "abcdefghijklmnopqrstuvwxyz";
     private static final String Numeric = "1234567890";
-    private static final String special_char = "@_";
+    private static final String special_char = "@_.";
     private static final String dic = caps + small_caps + Numeric + special_char;
 
     public String generatePassword(int len) {
